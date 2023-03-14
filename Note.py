62e38dc77f50319a0461adcc6b681d2103ed636f
rustam <rusitama@gmail.com>
2023-03-15 01:55:13 +0500

    Complite project

---
+++ Note.py
@@ -0,0 +1,12 @@
+import os
+os.system('cls')
+
+class Note:
+    def __init__(self, title, body, date, idarg):
+        self.title = title
+        self.body = body
+        self.date = date
+        self.id = idarg
+
+    def printNote(self):
+        return "ID: {}\nTitle: {}\n {}".format(self.id, self.title, self.body)
